104f8419213179b178cdd85568a7ccdf03eecd1d
sambartika <ec2-user@ip-172-31-18-51.us-east-2.compute.internal>
2018-10-11 02:33:37 +0000

    Second test rspec added

--- spec/controllers/admin/categories_controller_spec.rb
+++ spec/controllers/admin/categories_controller_spec.rb
@@ -51,6 +51,13 @@ describe Admin::CategoriesController do
       assert assigns(:category).valid?
       assigns(:categories).should_not be_nil
     end
+    
+    it "should edit category" do
+      post:edit, :category => {:name => "general", :keywords => "keys", :permalink => "new link"}
+      assert_response :redirect, :action=> "index"
+      expect(assigns(:category)).not_to be_nil
+      expect(flash[:notice]).to eq("Category was successfully saved.")
+    end
   end
 
   it "test_update" do
